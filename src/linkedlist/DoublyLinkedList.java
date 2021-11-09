e0f5e76ec126e509abb66ad7811da07b98a1a4a2
Kritika sharma <kritika.sharma_cs19@gla.ac.in>
2021-11-10 01:41:58 +0530

    created a DoublyLinkedList.java

---
+++ src/linkedlist/DoublyLinkedList.java
@@ -0,0 +1,18 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kritika Sharma
+ *   Date: 10-11-2021
+ *   Time: 01:40
+ *   File: DoublyLinkedList.java
+ */
+
+package linkedlist;
+
+public class DoublyLinkedList {
+    int data;
+    DoublyLinkedList previous;
+    DoublyLinkedList next;
+
+    public DoublyLinkedList(int data) {
+        this.data = data;
+    }
+}
