c55576f009c553b2c9a429425b2e8b749cba1a66
Kritika sharma <kritika.sharma_cs19@gla.ac.in>
2021-11-15 11:49:05 +0530

    implemented the stack class

---
+++ src/stack/Stack.java
@@ -0,0 +1,69 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kritika Sharma
+ *   Date: 15-11-2021
+ *   Time: 11:22
+ *   File: Stack.java
+ */
+
+package stack;
+
+import java.util.EmptyStackException;
+
+public class Stack {
+    private ListNode top;
+    private int length;
+    private class ListNode{
+        private int data;
+        private  ListNode next;
+
+        public ListNode(int data) {
+            this.data = data;
+            this.next=null;
+        }
+    }
+    public void push(int data){
+        ListNode temp =new ListNode(data);
+        temp.next=top;
+        top=temp;
+        length++;
+    }
+    public int pop(){
+        if(isEmpty()){
+            throw new EmptyStackException();
+        }
+        int result=top.data;
+        top=top.next;
+        length--;
+        return result;
+    }
+    public int peek(){
+        if(isEmpty()){
+            throw new EmptyStackException();
+        }
+        return top.data;
+    }
+    public Stack(){
+        top=null;
+        length=0;
+    }
+    public int length(){
+        return length;
+    }
+    public boolean isEmpty(){
+        return length==0;
+    }
+
+}
+class Main{
+    public static void main(String[] args) {
+      Stack stack= new Stack();
+      stack.push(10);
+        stack.push(15);
+        stack.push(20);
+        stack.push(30);
+        System.out.println(stack.peek());
+        stack.pop();
+        System.out.println(stack.peek());
+    }
+}
+
